8de298a131a3c165032a9d75f9515739f1ef2336
Alexandre do Vale <vale.apv@gmail.com>
2018-12-17 22:09:42 -0200

    Client (front)

--- server/index.js
+++ server/index.js
@@ -3,9 +3,15 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+app.use(express.static('client'));
+
 // http://localhost:6677/teste-rota
 app.get('/teste-rota', function(req, res) {
 	res.status(200).send('Rota de teste OK');
+});
+
+io.on('connection', function(socket){
+	console.log("IP:" + socket.handshake.address + " conectado.");
 });
 
 server.listen(6677, function(){
